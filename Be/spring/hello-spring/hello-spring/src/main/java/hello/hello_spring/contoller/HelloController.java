151c9dbaa66d3c2a3fbd1afd0a88aad629a93e05
승현 <tmdgusdl0245@naver.com>
2024-08-08 19:38:55 +0900

    finish MVC and Template Engine

--- Be/spring/hello-spring/hello-spring/src/main/java/hello/hello_spring/contoller/HelloController.java
+++ Be/spring/hello-spring/hello-spring/src/main/java/hello/hello_spring/contoller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.contoller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,4 +15,10 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    };
 }
